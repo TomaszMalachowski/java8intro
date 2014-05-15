94817f59df3199ff03ec21da4c331b8f9f16ece7
Kamil Berdychowski <k.berdychowski@gmail.com>
2014-05-15 21:17:29 +0200

    VolvoXC90 is arctic ready diesel - tests fixed

--- src/main/java/pl/pragmatists/defaults/VolvoXC90Arctic.java
+++ src/main/java/pl/pragmatists/defaults/VolvoXC90Arctic.java
@@ -3,4 +3,8 @@ package pl.pragmatists.defaults;
 //Rules of resolving default methods:
 //-- Implementations in classes, including abstract declarations, take precedence over all interface defaults.
 public class VolvoXC90Arctic implements DieselRunningCar {
+    @Override
+    public boolean accept(Fuel fuel) {
+        return fuel == Fuel.ArcticReadyDiesel;
+    }
 }
